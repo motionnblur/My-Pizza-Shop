0e98c001cb6b816cd50dc96855dd114b8e64f940
Can <carbonfiberdev@gmail.com>
2026-07-21 18:56:59 +0300

    Update InputManager.cs

--- Assets/Engineering/Scripts/Mono/Managers/InputManager.cs
+++ Assets/Engineering/Scripts/Mono/Managers/InputManager.cs
@@ -16,22 +16,18 @@ namespace Engineering.Scripts.Mono.Managers
         private InputAction lookAction;
         private InputAction attackAction;
         private InputAction interactAction;
-        private InputAction crouchAction;
-        private InputAction jumpAction;
         private InputAction previousAction;
         private InputAction nextAction;
         private InputAction sprintAction;
 
-        public Vector2 Move => moveAction.ReadValue<Vector2>();
-        public Vector2 Look => lookAction.ReadValue<Vector2>();
-
-        public bool AttackPressedThisFrame => attackAction.WasPressedThisFrame();
-        public bool InteractPressedThisFrame => interactAction.WasPressedThisFrame();
-        public bool CrouchPressedThisFrame => crouchAction.WasPressedThisFrame();
-        public bool JumpPressedThisFrame => jumpAction.WasPressedThisFrame();
-        public bool PreviousPressedThisFrame => previousAction.WasPressedThisFrame();
-        public bool NextPressedThisFrame => nextAction.WasPressedThisFrame();
-        public bool SprintIsHeld => sprintAction.IsPressed();
+        public event Action<Vector2> MoveChanged;
+        public event Action<Vector2> LookChanged;
+        public event Action AttackPressed;
+        public event Action InteractPerformed;
+        public event Action PreviousPressed;
+        public event Action NextPressed;
+        public event Action SprintStarted;
+        public event Action SprintCanceled;
 
         private void Awake()
         {
@@ -45,8 +41,6 @@ namespace Engineering.Scripts.Mono.Managers
             lookAction = playerActions.FindAction("Look", true);
             attackAction = playerActions.FindAction("Attack", true);
             interactAction = playerActions.FindAction("Interact", true);
-            crouchAction = playerActions.FindAction("Crouch", true);
-            jumpAction = playerActions.FindAction("Jump", true);
             previousAction = playerActions.FindAction("Previous", true);
             nextAction = playerActions.FindAction("Next", true);
             sprintAction = playerActions.FindAction("Sprint", true);
@@ -54,12 +48,74 @@ namespace Engineering.Scripts.Mono.Managers
 
         private void OnEnable()
         {
+            moveAction.performed += OnMoveChanged;
+            moveAction.canceled += OnMoveChanged;
+            lookAction.performed += OnLookChanged;
+            lookAction.canceled += OnLookChanged;
+            attackAction.performed += OnAttackPerformed;
+            interactAction.performed += OnInteractPerformed;
+            previousAction.performed += OnPreviousPerformed;
+            nextAction.performed += OnNextPerformed;
+            sprintAction.started += OnSprintStarted;
+            sprintAction.canceled += OnSprintCanceled;
+
             playerActions?.Enable();
         }
 
         private void OnDisable()
         {
             playerActions?.Disable();
+
+            moveAction.performed -= OnMoveChanged;
+            moveAction.canceled -= OnMoveChanged;
+            lookAction.performed -= OnLookChanged;
+            lookAction.canceled -= OnLookChanged;
+            attackAction.performed -= OnAttackPerformed;
+            interactAction.performed -= OnInteractPerformed;
+            previousAction.performed -= OnPreviousPerformed;
+            nextAction.performed -= OnNextPerformed;
+            sprintAction.started -= OnSprintStarted;
+            sprintAction.canceled -= OnSprintCanceled;
+        }
+
+        private void OnMoveChanged(InputAction.CallbackContext context)
+        {
+            MoveChanged?.Invoke(context.ReadValue<Vector2>());
+        }
+
+        private void OnLookChanged(InputAction.CallbackContext context)
+        {
+            LookChanged?.Invoke(context.ReadValue<Vector2>());
+        }
+
+        private void OnAttackPerformed(InputAction.CallbackContext context)
+        {
+            AttackPressed?.Invoke();
+        }
+
+        private void OnInteractPerformed(InputAction.CallbackContext context)
+        {
+            InteractPerformed?.Invoke();
+        }
+
+        private void OnPreviousPerformed(InputAction.CallbackContext context)
+        {
+            PreviousPressed?.Invoke();
+        }
+
+        private void OnNextPerformed(InputAction.CallbackContext context)
+        {
+            NextPressed?.Invoke();
+        }
+
+        private void OnSprintStarted(InputAction.CallbackContext context)
+        {
+            SprintStarted?.Invoke();
+        }
+
+        private void OnSprintCanceled(InputAction.CallbackContext context)
+        {
+            SprintCanceled?.Invoke();
         }
     }
 }
