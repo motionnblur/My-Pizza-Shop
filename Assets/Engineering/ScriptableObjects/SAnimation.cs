efc02ed680729fe9af1be12e5ca9e678e47f1d49
Can <carbonfiberdev@gmail.com>
2026-07-23 02:51:30 +0300

    Add SAnimation

---
+++ Assets/Engineering/ScriptableObjects/SAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Engineering.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "SAnimation", menuName = "Scriptables/SAnimation", order = 0)]
+    public class SAnimation : ScriptableObject
+    {
+        public float moneySpendDelay = 1f;
+        public float moneySpendSpeed = 1f;
+        public float jumpPower = 3f;
+        public float duration = 0.5f;
+        public float rotationAmount = 360f;
+
+        public float randomOffsetRadius = 0.5f;
+        
+        public DG.Tweening.Ease moveEase = DG.Tweening.Ease.OutQuad;
+    }
+}
