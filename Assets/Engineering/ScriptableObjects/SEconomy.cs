216409dc9cd992d465632aec87f52c439fe82ee1
Can <carbonfiberdev@gmail.com>
2026-07-23 03:10:28 +0300

    Adding ui manager...

--- Assets/Engineering/ScriptableObjects/SEconomy.cs
+++ Assets/Engineering/ScriptableObjects/SEconomy.cs
@@ -5,6 +5,7 @@ namespace Engineering.ScriptableObjects
     [CreateAssetMenu(fileName = "SEconomy", menuName = "Scriptables/SEconomy", order = 0)]
     public class SEconomy : ScriptableObject
     {
+        public int totalPlayerMoney = 250;
         public int playerMoneySpendRate = 5;
         public GameObject moneyPrefab;
     }
---
+++ Assets/Engineering/Scripts/Mono/Managers/UIManager.cs
@@ -0,0 +1,10 @@
+﻿using Engineering.ScriptableObjects;
+using UnityEngine;
+
+namespace Engineering.Scripts.Mono.Managers
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField] private SEconomy sEconomy;
+    }
+}
