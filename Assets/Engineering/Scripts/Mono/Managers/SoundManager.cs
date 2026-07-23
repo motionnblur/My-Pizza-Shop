686a0cccd5782b73effd2d9fe1425e034291c002
Can <carbonfiberdev@gmail.com>
2026-07-23 15:30:19 +0300

    Setup sound system

---
+++ Assets/Engineering/Scripts/Mono/Managers/SoundManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace Engineering.Scripts.Mono.Managers
+{
+    public class SoundManager : MonoBehaviour
+    {
+        public static SoundManager Instance { get; private set; }
+
+        [SerializeField] private AudioSource sfxSource;
+        [SerializeField] private AudioSource musicSource;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void PlaySFX(AudioClip clip)
+        {
+            if (clip != null)
+                sfxSource.PlayOneShot(clip);
+        }
+
+        public void PlayMusic(AudioClip clip, bool loop = true)
+        {
+            if (clip == null) return;
+            musicSource.clip = clip;
+            musicSource.loop = loop;
+            musicSource.Play();
+        }
+
+        public void StopMusic()
+        {
+            musicSource.Stop();
+        }
+
+        public void SetSFXVolume(float volume)
+        {
+            sfxSource.volume = Mathf.Clamp01(volume);
+        }
+
+        public void SetMusicVolume(float volume)
+        {
+            musicSource.volume = Mathf.Clamp01(volume);
+        }
+    }
+}
